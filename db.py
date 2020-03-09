90f0cc7c56ec3bb7d131ae47b352479a246533b5
Eric Pinero <ericpinero@gmail.com>
2020-03-09 12:40:20 -0400

    write paints images and colors to db

--- db.py
+++ db.py
@@ -1,8 +1,13 @@
 import pymongo
 from pymongo import MongoClient
+import json
 
 cluster =  MongoClient("mongodb+srv://admin-db:g9LdLMiYf5mwvoDl@cluster0-ioxp7.mongodb.net/test?retryWrites=true&w=majorityp")
 
 db = cluster["color-schemer"]
 collection = db["acryllic-paints"]
 
+with open('/Users/mukkan/Desktop/personal_projects/color-schemer/testscrape/paints.json') as f:
+    data = json.load(f)
+
+collection.insert(data[0])
