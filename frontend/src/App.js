5470632edb3b6ddb54b3aa5d6c76969d2e30a590
Eric Pinero <ericpinero@gmail.com>
2020-03-27 12:40:57 -0400

    add react color wheel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ColourWheel from 'react-colour-wheel'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to color-schemer
         </p>
         <a
           className="App-link"
@@ -19,6 +20,21 @@ function App() {
           Learn React
         </a>
       </header>
+      <ColourWheel
+        radius={200}
+        padding={10}
+        lineWidth={50}
+        onColourSelected={(rgb) => this.setState({ rgb })}
+        onRef={ref => (this.colourWheel = ref)}
+        spacers={{
+          colour: '#FFFFFF',
+          shadowColour: 'grey',
+          shadowBlur: 5
+        }}
+        preset
+        presetColour={this.state.rgb}
+        animated
+      />
     </div>
   );
 }
