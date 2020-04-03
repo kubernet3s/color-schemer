7ca5d6bb95463bf2af4fbbeba1e56c7c41e23dec
Eric Pinero <ericpinero@gmail.com>
2020-04-03 13:51:08 -0400

    tested complementary color functionality succesfully

--- frontend/src/components/complementaryColors.js
+++ frontend/src/components/complementaryColors.js
@@ -16,11 +16,12 @@ function complementLocator(color, colorList){
     for (let i = 0; i < colorList.length; i++){
         const paint = colorList[i]
         const rgb = paint.rgba.slice(0, 3)
-
         let delta = 0
+
         for (let i = 0; i < rgb.length; i++) {
             const element = rgb[i];
             delta += Math.abs(trueComplement[i] - element)
+            debugger
         }
         if (delta < leastDelta){
             leastDelta = delta
