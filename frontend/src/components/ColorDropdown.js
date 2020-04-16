0683a5dee1c2728e2a17bd333f6f4d61019eabc5
Eric Pinero <ericpinero@gmail.com>
2020-04-15 22:54:53 -0400

    experimenting with react-select styles

--- frontend/src/components/ColorDropdown.js
+++ frontend/src/components/ColorDropdown.js
@@ -20,10 +20,61 @@ const ColorDropdown = ({scheme, schemeChange}) => {
         schemeChange(change)
     }
 
+    const dot = (color = '#ccc') => ({
+        alignItems: 'center',
+        display: 'flex',
+      
+        ':after': {
+          backgroundColor: color,
+          border: "1px solid black",
+          borderRadius: 10,
+          content: '" "',
+          display: 'block',
+          marginRight: 8,
+          height: 10,
+          width: 10,
+        },
+      });
+
+      const customStyles = {
+        control: styles => ({ ...styles, backgroundColor: 'white' }),
+        option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+          const color = data.value.rgba.join(",").slice(0,3);
+          debugger
+          return {
+            ...styles,
+            backgroundColor: isDisabled
+              ? null
+              : isSelected
+              ? color
+              : isFocused
+              ? color
+              : null,
+            color: isDisabled
+              ? '#ccc'
+              : isSelected
+              ? 'white'
+                ? 'white'
+                : 'black'
+              : color,
+            cursor: isDisabled ? 'not-allowed' : 'default',
+      
+            ':active': {
+              ...styles[':active'],
+              backgroundColor: !isDisabled && (isSelected ? color : color),
+            },
+          };
+        },
+        input: styles => ({ ...styles, ...dot() }),
+        placeholder: styles => ({ ...styles, ...dot() }),
+        singleValue: (styles, { data }) => ({ ...styles, ...dot(data.value.rgba.join(",")) }),
+      };    debugger
+
     return(
         <Select 
             options = {list}
             closeMenuOnSelect = {false}
+            styles = {customStyles}
             isMulti
             components = {makeAnimated()}
             onChange = {handleChange}
