cb60876fd77079fb3e4f01f6dab4aeaed93c3132
Eric Pinero <ericpinero@gmail.com>
2020-07-01 17:15:15 -0400

    center footer links

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,10 +3,12 @@ import React from 'react';
 const Footer = () =>{
 
     return(
-        <footer className="bg-red padding-20 flex default-margin space-between bold">
-            <a href="https://www.thearmypainter.com/" className="white f-20">The Army Painter</a>
-            <a href="https://github.com/eric-pinero" className="white f-20">Github</a>
-            <a href="https://www.ericpcodes.com/" className="white f-20">Portfolio</a>
+        <footer className="bg-red padding-20 flex default-margin bold justify-center">
+            <nav className="w-80percent flex space-around">
+                <a href="https://www.thearmypainter.com/" className="white f-20">The Army Painter</a>
+                <a href="https://github.com/eric-pinero" className="white f-20">Github</a>
+                <a href="https://www.ericpcodes.com/" className="white f-20 margin-l-7percent">Portfolio</a>
+            </nav>
         </footer>
     )
 };
