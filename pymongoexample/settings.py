de52370fc800058da0e621c1852e98691faf410e
Eric Pinero <ericpinero@gmail.com>
2020-07-03 21:47:49 -0400

    implementation attempt with rails 5.2.3

--- pymongoexample/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-# from os.path import join, dirname
-# from dotenv import load_dotenv
-
-# dotenv_path = join(dirname(__file__), '.env')
-# load_dotenv(dotenv_path)
-MONGO_URI = os.environ.get('MONGO_URI')
-print(MONGO_URI)
