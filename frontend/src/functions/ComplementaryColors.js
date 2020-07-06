5ae0ee5000ed02ed6600eb81ccf703e4346fe5d7
kubernet3s <milojruss@gmail.com>
2020-07-06 09:14:14 -0400

    Update ComplementaryColors.js

--- frontend/src/functions/ComplementaryColors.js
+++ frontend/src/functions/ComplementaryColors.js
@@ -1,10 +1,11 @@
-function complementCalculator(color){
-    let complement = [0, 0, 0]
+import colorsys as cs
 
-    for (let i = 0; i < color.length; i++) {
-        const element = color[i];
-        complement[i] = 255 - element
-    }
+function complementCalculator(color){
+    r = color[0]
+    g = color[1]
+    b = color[2]
+    rawcomp = cs.hsv_to_rgb((cs.rgb_to_hsv(r,g,b)[0]+.5)%1,cs.rgb_to_hsv(r,g,b)[1],cs.rgb_to_hsv(r,g,b)[2])
+    complement = [round(num, 0) for num in rawcomp] 
     return complement
 }
 
